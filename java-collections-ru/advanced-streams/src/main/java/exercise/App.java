3175f5166f0a8fa1ca042e14a4ea044a8d7b3f89
@hexlet/cli <support@hexlet.io>
2023-07-26 17:51:37 +0300

    submit java-collections-ru/advanced-streams

---
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+// BEGIN
+class App {
+    public static String getForwardedVariables(String file) {
+
+        return (String) Arrays.stream(file.split("\n"))
+                .filter(s -> s.startsWith("environment"))
+                .map(s -> s.replaceAll("environment=", ""))
+                .map(s -> s.replaceAll("\"", ""))
+                .map(s -> s.replaceAll("language=en", ""))
+                .map(s -> s.replaceAll(",", " "))
+                .map(s -> s.split(" "))
+                .flatMap(Arrays::stream)
+                .filter(s -> s.startsWith("X_FORWARDED_"))
+                .map(s -> s.replaceAll("X_FORWARDED_", ""))
+                .map(s -> s.trim())
+                .peek(s -> System.out.println(s))
+                .collect(Collectors.joining(","));
+    }
+}
+
+//END
