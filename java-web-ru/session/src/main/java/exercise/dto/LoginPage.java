481b7679c85a11a1cafc62e4e64a99c97032603f
@hexlet/cli <support@hexlet.io>
2024-01-26 13:35:50 +0300

    submit java-web-ru/session

---
+++ java-web-ru/session/src/main/java/exercise/dto/LoginPage.java
@@ -0,0 +1,15 @@
+package exercise.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class LoginPage {
+    private String name;
+    private String error;
+
+    public LoginPage() {
+
+    }
+}
